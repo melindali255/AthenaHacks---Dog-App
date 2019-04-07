d93a47cfb1e90420815ecd70ef55b12b8f62e720
sthiem <49344595+sthiem@users.noreply.github.com>
2019-04-07 11:03:08 -0700

    add title and less redundant code

--- main.py
+++ main.py
@@ -16,17 +16,18 @@ _DEFAULT_FONT = ('Helvetica', 20)
 class DogApp:
     def __init__(self):
         self._root_window = tkinter.Tk()
+        self._root_window.title("Cheer Up Cutie Py")
         self._create_button()
 
         self._root_window.minsize(600,600)
         self._root_window.configure(background="#FF9E91")
 
-    def _create_button(self):
+    def _create_button(self, txt = "CLICK HERE IF YOU'RE SAD", px = 0, py = 250):
         self.button = tkinter.Button(
-            master = self._root_window, text = "CLICK HERE IF YOU'RE SAD",
+            master = self._root_window, text = txt,
             font = _DEFAULT_FONT,
             command = self.handle_click)
-        self.button.pack(padx = 0, pady = 250) 
+        self.button.pack(padx = px, pady = py) 
         return self.button
 
     def handle_click(self):
@@ -44,9 +45,7 @@ class DogApp:
         self.canvas.pack()
         self.canvas.create_image((250,250), image=tkimage)
         self.canvas.img = tkimage
-        self.button = tkinter.Button(master = self._root_window,text = "STILL SAD?", 
-                                     font = _DEFAULT_FONT, command = self.handle_click)
-        self.button.pack()
+        self.button = self._create_button(txt = "STILL SAD?", px=0, py=0)
 
     def run(self):
         self._root_window.mainloop()
